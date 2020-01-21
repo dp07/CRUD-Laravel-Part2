d1c8df3934c729a13176a4a40c7e25feecce4abc
dp07 <dp.madridiztazc@gmail.com>
2020-01-21 14:24:28 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('home');
+});
 
-Route::get('/', 'SiswaController@index');
+// Route::get('/', 'SiswaController@index');
+Route::get('/dashboard', 'DashboardController@index');
 Route::get('/siswa', 'SiswaController@index');
 Route::post('/siswa/create', 'SiswaController@create');
 Route::get('/siswa/{id}/edit', 'SiswaController@edit');
